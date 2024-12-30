032c1dc47d1d333188b37ef4802c15a0ecc8e9e9
metziger23 <metziger23@gmail.com>
2024-12-30 23:39:02 +0300

    back to custom_config + same timeout

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,4 +1,23 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-#include "custom_combos.dtsi"
+#include "miryoku.h"
+#include <behaviors.dtsi>
+#include <dt-bindings/zmk/keys.h>
+
+/ {
+    combos {
+        compatible = "zmk,combos";
+        combo_esc {
+            timeout-ms = <50>;
+            key-positions = <3 13>;
+            bindings = <&lt U_MEDIA ESC>;
+        };
+        combo_del {
+            timeout-ms = <50>;
+            key-positions = <6 16>;
+            bindings = <&lt U_FUN DEL>;
+        };
+    };
+};
+
