cbdf6813aaada58710beffb1be242013f46e03b5
metziger23 <metziger23@gmail.com>
2024-12-30 22:02:56 +0300

    added esc and del combos

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,18 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+/ {
+    combos {
+        compatible = "zmk,combos";
+        combo_esc {
+            timeout-ms = <50>;
+            key-positions = <3 13>;
+            bindings = <&kp ESC>;
+        };
+        combo_del {
+            timeout-ms = <50>;
+            key-positions = <6 16>;
+            bindings = <&kp DEL>;
+        };
+    };
+};
