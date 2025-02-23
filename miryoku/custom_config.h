51187c4c0e0dd1008fd4a22dc391cab55d861d26
metziger23 <metziger23@gmail.com>
2025-02-23 23:25:21 +0300

    top horizontal combos for esc and del
    alignment only
    
    changed combos from vertical to top
    
    U_LT are on the top

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -23,6 +23,18 @@ U_NP,              U_NP,              U_NA,              &kp LSHFT,         &kp 
 &kp F10,           &kp F1,            &kp F2,            &kp F3,            &kp PAUSE_BREAK,   U_NA,              &u_to_U_FUN,       &u_to_U_MEDIA,     &kp RALT,          U_NA,              \
 U_NP,              U_NP,              &kp K_APP,         &kp SPACE,         &kp TAB,           &kp LSHFT,         &kp LSHFT,         U_NA,              U_NP,              U_NP
 
+#define MIRYOKU_LAYER_BASE \
+&kp Q,             &kp W,             &kp F,             U_LT(U_MEDIA, P),  &kp B,             &kp J,             U_LT(U_FUN, L),    &kp U,             &kp Y,             &kp SQT,           \
+U_MT(LGUI, A),     U_MT(LALT, R),     U_MT(LCTRL, S),    U_MT(LSHFT, T),    &kp G,             &kp M,             U_MT(LSHFT, N),    U_MT(LCTRL, E),    U_MT(LALT, I),     U_MT(LGUI, O),     \
+U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
+U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
+
+#define MIRYOKU_LAYER_EXTRA \
+&kp Q,             &kp W,             &kp E,             U_LT(U_MEDIA, R),  &kp T,             &kp Y,             U_LT(U_FUN, U),    &kp I,             &kp O,             &kp P,             \
+U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SQT),   \
+U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
+U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
+
 #define COMBO(NAME, BINDINGS, KEYPOS, LAYERS, TERM) \
 combo_##NAME { \
     timeout-ms = <TERM>; \
@@ -34,18 +46,18 @@ combo_##NAME { \
 / {
     combos {
         compatible = "zmk,combos";
-        COMBO(base_left,    &lt U_MEDIA ESC, 3 13, U_BASE,   50)
-        COMBO(extra_left,   &lt U_MEDIA ESC, 3 13, U_EXTRA,  50)
-        COMBO(button_left,  U_BTN3,          3 13, U_BUTTON, 50)
-        COMBO(num_left,     &kp DOT,         3 13, U_NUM,    50)
-        COMBO(sym_left,     &kp LPAR,        3 13, U_SYM,    50)
-        COMBO(fun_left,     &kp K_APP,       3 13, U_FUN,    50)
-        COMBO(base_right,   &lt U_FUN DEL,   6 16, U_BASE,   50)
-        COMBO(extra_right,  &lt U_FUN DEL,   6 16, U_EXTRA,  50)
-        COMBO(button_right, U_BTN3,          6 16, U_BUTTON, 50)
-        COMBO(nav_right,    &kp DEL,         6 16, U_NAV,    50)
-        COMBO(mouse_right,  U_BTN3,          6 16, U_MOUSE,  50)
-        COMBO(media_right,  &kp C_MUTE,      6 16, U_MEDIA,  50)
+        COMBO(base_left,    &kp ESC,    2 3, U_BASE,   50)
+        COMBO(extra_left,   &kp ESC,    2 3, U_EXTRA,  50)
+        COMBO(button_left,  U_BTN3,     2 3, U_BUTTON, 50)
+        COMBO(num_left,     &kp DOT,    2 3, U_NUM,    50)
+        COMBO(sym_left,     &kp LPAR,   2 3, U_SYM,    50)
+        COMBO(fun_left,     &kp K_APP,  2 3, U_FUN,    50)
+        COMBO(base_right,   &kp DEL,    6 7, U_BASE,   50)
+        COMBO(extra_right,  &kp DEL,    6 7, U_EXTRA,  50)
+        COMBO(button_right, U_BTN3,     6 7, U_BUTTON, 50)
+        COMBO(nav_right,    &kp DEL,    6 7, U_NAV,    50)
+        COMBO(mouse_right,  U_BTN3,     6 7, U_MOUSE,  50)
+        COMBO(media_right,  &kp C_MUTE, 6 7, U_MEDIA,  50)
     };
 };
 
